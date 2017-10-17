61f0ab8bcdf8f3fef81296b345e6638c3ae94f41
marc gugliuzza <>
2017-10-16 22:12:24 -0400

    uploading working, single store page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  // where to store?
+  storage: multer.memoryStorage(),
+  fileFilter (req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed' }, false);
+    }
+  }
+  // what kinds of follows allowed?
+};
 
 
 exports.homepage = (req, res) => {
@@ -10,6 +27,32 @@ exports.homepage = (req, res) => {
 exports.addStore = (req, res) => {
 	res.render('editStore', {title: 'Add Store'});
 };
+
+// middleware to handle upload
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // if no file..
+  if (!req.file) {
+    next();
+    return;
+  }
+  console.log(req.file);
+  const extension = req.file.mimetype.split('/')[1];
+
+  // get info ready for createStore
+  req.body.photo = `${uuid.v4()}.${extension}`;
+
+  // resize
+  const photo = await jimp.read(req.file.buffer)
+  await photo.resize(800, jimp.AUTO);
+
+  // save it
+  await photo.write(`./public/uploads/${req.body.photo}`);
+
+  // once it is saved to filesystem, keep going..
+  next();
+}
 
 exports.createStore = async (req, res) => {
   // save this data back to the database
@@ -58,4 +101,17 @@ exports.getStores = async (req, res) => {
   console.log(stores);
 
   res.render('stores', {title: `Stores`, stores: stores}); 
+}
+
+exports.getStoreBySlug = async (req, res,  next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+
+  if (!store) {
+    return next();
+  }
+
+  res.render('store', {store: store, title: store.name});
+  //res.json(store);
+  //res.send('it works');
+
 }
